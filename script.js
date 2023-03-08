1957570c12fd0f1467877fe78f314c062345f5b8
Shivam Sehgal <shs604813@gmail.com>
2023-03-08 21:10:08 +0530

    Improve UI and styling our App Web Page

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let btn = document.getElementById("menu-btn");
 let nav = document.getElementById("menu");
+let rotate = document.querySelectorAll("#svg-btn");
 
 function navToggle() {
   btn.classList.toggle("open");
@@ -7,4 +8,30 @@ function navToggle() {
   document.body.classList.toggle("no-scroll");
 }
 
+function rotateSvgFirst() {
+  rotate[0].classList.toggle("rotate-svg-up");
+}
+
+function rotateSvgSecond() {
+  rotate[1].classList.toggle("rotate-svg-up");
+}
+
+function rotateSvgThird() {
+  rotate[2].classList.toggle("rotate-svg-up");
+}
+
+function rotateSvgFourth() {
+  rotate[3].classList.toggle("rotate-svg-up");
+}
+
+function rotateSvgFifth() {
+  rotate[4].classList.toggle("rotate-svg-up");
+}
+
 btn.addEventListener("click", navToggle);
+
+rotate[0].addEventListener("click", rotateSvgFirst);
+rotate[1].addEventListener("click", rotateSvgSecond);
+rotate[2].addEventListener("click", rotateSvgThird);
+rotate[3].addEventListener("click", rotateSvgFourth);
+rotate[4].addEventListener("click", rotateSvgFifth);
